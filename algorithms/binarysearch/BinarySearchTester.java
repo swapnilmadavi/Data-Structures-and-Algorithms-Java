2659b8cbf09169c0900b0e22b45bb255aecb4082
Swapnil Madavi <swapy.madavi04@gmail.com>
2022-02-02 22:36:16 +0530

    Add testcases for Binary search

--- algorithms/binarysearch/BinarySearchTester.java
+++ algorithms/binarysearch/BinarySearchTester.java
@@ -1,44 +1,52 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.util.StringJoiner;
 
 public class BinarySearchTester {
-    private static final String TEST_FILE = "tc_binary_search.txt";
 
     public static void main(String[] args) {
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(TEST_FILE))) {
-            String line;
-            String[] testCase;
-            int[] inputArray;
-            int result;
-            
-            while ((line = bufferedReader.readLine()) != null) {
-                testCase = line.split(" ");
-                inputArray = parseIntArray(testCase[0]);
-                result = BinarySearch.search(inputArray, Integer.parseInt(testCase[1]));
-                System.out.println("Input array: " + testCase[0]);
-                System.out.println("Element: " + testCase[1]);
-                System.out.println("Expected output: " + testCase[2]);
-                System.out.println("Result: " + result);
-                System.out.println();
-            }
-        } catch (IOException exception) {
-            exception.printStackTrace();
-        }
+        testCase1();
+        testCase2();
+        testCase3();
     }
 
-    private static int[] parseIntArray(String arrayString) {
-        String[] arrayElements = arrayString.substring(1, arrayString.length() - 1).split(",");
-        
-        // Quick & Hacky way to handle empty array
-        if (arrayElements.length == 1 && arrayElements[0].isEmpty()) {
-            return new int[0];
+    private static void testCase1() {
+        int[] inputArray = new int[] {3, 4, 5, 6, 7, 8, 9};
+        int element = 5;
+        int expectedOutput = 2;
+        int output = BinarySearch.search(inputArray, element);
+        printResult(inputArray, element, expectedOutput, output);
+    }
+
+    private static void testCase2() {
+        int[] inputArray = new int[] {11, 14, 25, 30, 40, 41, 52, 57, 70};
+        int element = 23;
+        int expectedOutput = -1;
+        int output = BinarySearch.search(inputArray, element);
+        printResult(inputArray, element, expectedOutput, output);
+    }
+
+    private static void testCase3() {
+        int[] inputArray = new int[] {};
+        int element = 100;
+        int expectedOutput = -1;
+        int output = BinarySearch.search(inputArray, element);
+        printResult(inputArray, element, expectedOutput, output);
+    }
+
+    private static void printResult(int[] inputArray, int element, int expectedOutput, int output) {
+        System.out.println("Input array: " + stringifyArray(inputArray));
+        System.out.println("Element: " + element);
+        System.out.println("Expected output: " + expectedOutput);
+        System.out.println("Output: " + output);
+        System.out.println();
+    }
+
+    private static String stringifyArray(int[] array) {
+        StringJoiner stringJoiner = new StringJoiner(", ", "{", "}");
+
+        for (int i : array) {
+            stringJoiner.add(Integer.toString(i));
         }
 
-        int[] intArray = new int[arrayElements.length];
-        for (int i = 0; i < arrayElements.length; i++) {
-            intArray[i] = Integer.parseInt(arrayElements[i]);
-        }
-        return intArray;
+        return stringJoiner.toString();
     }
 }
