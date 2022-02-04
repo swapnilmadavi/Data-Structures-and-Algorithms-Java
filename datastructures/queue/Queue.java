5fe3fecb8ba62ec13c4abbfcdef2f9fdc6b53abf
Swapnil Madavi <swapy.madavi04@gmail.com>
2022-02-04 20:58:03 +0530

    Add tester class for Queue

--- datastructures/queue/Queue.java
+++ datastructures/queue/Queue.java
@@ -1,49 +1,75 @@
-public class Queue<T> {
-	private int capacity;
-	private T[] array;
-	private int front, rear;
-	private int length;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-	public Queue(int capacity) {
-		if (capacity <= 0) throw new IllegalArgumentException("must be greater than 0");
-		this.capacity = capacity;
-		array = (T[]) new Object[capacity];
-		rear = -1;
-	}
+public class Queue<T> implements Iterable<T> {
+    private int capacity;
+    private T[] array;
+    private int front, rear;
+    private int length;
 
-	public Queue() {
-		this(10);
-	}
+    public Queue(int capacity) {
+        if (capacity <= 0) throw new IllegalArgumentException("must be greater than 0");
+        this.capacity = capacity;
+        array = (T[]) new Object[capacity];
+        rear = -1;
+    }
 
-	public void enqueue(T element) {
-		if (isFull()) throw new RuntimeException("Queue is full");
-		rear = (rear + 1) % capacity;
-		array[rear] = element;
-		length++;
-	}
+    public Queue() {
+        this(10);
+    }
 
-	public T dequeue() {
-		if (isEmpty()) throw new RuntimeException("Queue already empty");
-		T element = array[front];
-		front = (front + 1) % capacity;
-		length--;
-		return element;
-	}
+    public void enqueue(T element) {
+        if (isFull()) throw new RuntimeException("Queue is full");
+        rear = (rear + 1) % capacity;
+        array[rear] = element;
+        length++;
+    }
 
-	public T peek() {
-		if (isEmpty()) throw new RuntimeException("Queue is empty");
-		return array[front];
-	}
+    public T dequeue() {
+        if (isEmpty()) throw new RuntimeException("Queue already empty");
+        T element = array[front];
+        front = (front + 1) % capacity;
+        length--;
+        return element;
+    }
 
-	public int size() {
-		return length;
-	}
+    public T peek() {
+        if (isEmpty()) throw new RuntimeException("Queue is empty");
+        return array[front];
+    }
 
-	public boolean isFull() {
-		return size() == capacity;
-	}
+    public int size() {
+        return length;
+    }
 
-	public boolean isEmpty() {
-		return size() == 0;
-	}
+    public boolean isFull() {
+        return size() == capacity;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int i = 0;
+
+            @Override
+            public boolean hasNext() {
+                return i < length;
+            }
+
+            @Override
+            public T next()
+            {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                T element = array[(front + i) % capacity];
+                i++;
+                return element;
+            }
+        };
+    }
 }
--- datastructures/queue/QueueTester.java
+++ datastructures/queue/QueueTester.java
@@ -1,25 +1,42 @@
+import java.util.StringJoiner;
+
 public class QueueTester {
-	public static void main(String[] args) {
-		Queue<Integer> queue = new Queue<>(3);
-		
-		System.out.println("Queue initialize => 12 7");
-		queue.enqueue(12);
-		queue.enqueue(7);
-		
-		System.out.println("Queue size => " + queue.size());
-		System.out.println("Queue front => " + queue.peek());
-		System.out.println("Queue dequeue => " + queue.dequeue());
-		
-		System.out.println("Queue enqueue 29");
-		queue.enqueue(29);
-		
-		System.out.println("Queue enqueue 41");
-		queue.enqueue(41);
-		
-		System.out.println("Queue size => " + queue.size());
-		System.out.println("Queue front => " + queue.peek());
-		
-		System.out.println("Trying to enqueue 38 in an already full queue; should throw an Exception");
-		queue.enqueue(38);
-	}
+    public static void main(String[] args) {
+        Queue<Integer> queue = new Queue<>(4);
+        queue.enqueue(12);
+        queue.enqueue(41);
+        queue.enqueue(29);
+        System.out.println("Initial queue => " + stringifyQueue(queue));
+        System.out.println();
+        
+        // Enqueue operation
+        queue.enqueue(7);
+        System.out.println("Enqueue 7 => " + stringifyQueue(queue));
+        System.out.println("Size after inserting 7 => " + queue.size());
+        System.out.println();
+        
+        // Dequeue operation
+        System.out.println("Element at front => " + queue.peek());
+        System.out.println("Dequeue => " + queue.dequeue());
+        System.out.println("Queue after dequeue operation => " + stringifyQueue(queue));
+        System.out.println();
+
+        queue.enqueue(38);
+        System.out.println("Enqueue 38 => " + stringifyQueue(queue));
+        System.out.println();
+
+        // Queue full exception
+        System.out.println("Trying to enqueue 12 again in an already full queue; should throw an Exception");
+        queue.enqueue(12);
+    }
+
+    private static <T> String stringifyQueue(Queue<T> queue) {
+        StringJoiner stringJoiner = new StringJoiner(", ", "[", "]");
+
+        for (T element : queue) {
+            stringJoiner.add(element.toString());
+        }
+
+        return stringJoiner.toString();
+    }
 }
